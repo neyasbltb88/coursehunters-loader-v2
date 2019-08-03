c90a58122dbfbefa8223a12195af81f601fc1ac8
Neyasbltb <neyasbltb_88@mail.ru>
2019-08-03 14:44:56 +0300

    Восстановлена работа сборщика страницы с информацией о курсе

--- app/scripts/info-page-collector.js
+++ app/scripts/info-page-collector.js
@@ -1,20 +1,26 @@
-import fonts from './fonts';
+import fonts from "./fonts";
 
 export default class InfoPageCollector {
     constructor() {
-        let owner_link = document.querySelector('a.course-box-value');
+        let owner_link = document.querySelector("a.course-box-value");
         this.owner = {
             name: owner_link.textContent,
             url: owner_link.href
         };
-        this.course_cover = document.querySelector('.lessons-item [itemprop="thumbnail"]').href;
-        this.course_name = document.querySelector('h1.hero-title').textContent;
-        this.course_info_script = document.querySelector('[type="application/ld+json"]').textContent;
+        this.course_cover = document.querySelector(
+            '.lessons-item [itemprop="thumbnail"]'
+        ).href;
+        this.course_name = document.querySelector("h1.hero-title").textContent;
+        this.course_info_script = document.querySelector(
+            '[type="application/ld+json"]'
+        ).textContent;
         this.course_info_script ? JSON.parse(this.course_info_script) : null;
     }
 
     async resourceUrl2base64(url) {
-        let res = await window.Loader.request(url, { responseType: 'blob' });
+        let res = await window.Loader.request(url, {
+            responseType: "blob"
+        });
         let b64 = await window.Utils.blob2base64(res.target.response);
 
         return b64;
@@ -23,7 +29,7 @@ export default class InfoPageCollector {
     async fontsCollector(fonts) {
             let fonts_style = ``;
 
-            let loadFont = async(font_obj) => {
+            let loadFont = async font_obj => {
                     let font_name = font_obj.name;
                     let font_weight = font_obj.weight;
                     let font_style = font_obj.style;
@@ -34,18 +40,18 @@ export default class InfoPageCollector {
             @font-face {
                 font-family: ${font_name};
                 src: url("${font}");
-                ${font_weight ? `font-weight: ${font_weight};` : ''}
-                ${font_style ? `font-style: ${font_style};` : ''}
+                ${font_weight ? `font-weight: ${font_weight};` : ""}
+                ${font_style ? `font-style: ${font_style};` : ""}
             }`;
-        }
+        };
 
-        let loopLoadFonts = async(index = 0) => {
+        let loopLoadFonts = async (index = 0) => {
             let font_obj = fonts[index];
             if (!font_obj) return fonts_style;
 
             fonts_style += await loadFont(font_obj);
             return await loopLoadFonts(++index);
-        }
+        };
 
         await loopLoadFonts(0);
 
@@ -65,7 +71,7 @@ export default class InfoPageCollector {
 
             <style>
                 /* Родные стили страницы */
-                ${document.querySelector('head style').textContent}
+                ${document.querySelector("head style").textContent}
 
                 /* Инлайновые шрифты */
                 ${await this.fontsCollector(fonts)}
@@ -166,19 +172,27 @@ export default class InfoPageCollector {
             
             <!-- Шапка. На градиенте хлебные крошки и заголовки -->
             ${await (async () => {
-                let header = document.querySelector('.hero.hero-gradient').cloneNode(true);
+                let header = document
+                    .querySelector(".hero.hero-gradient")
+                    .cloneNode(true);
 
                 // Сделать активным пункт в хлебных крошках, указывающий на эту страницу
-                let breadcrumbs_active = header.querySelector('.breadcrumbs__a_active');
+                let breadcrumbs_active = header.querySelector(
+                    ".breadcrumbs__a_active"
+                );
                 breadcrumbs_active.innerHTML = /* html */ `
-                    <a class="breadcrumbs__a" itemprop="item" href="${document.location.href}">
+                    <a class="breadcrumbs__a" itemprop="item" href="${
+                        document.location.href
+                    }">
                         <span itemprop="name">${this.course_name}</span>
                     </a>`;
 
                 // Сделать активной ссылку на все курсы автора
-                let hero_source = header.querySelector('.hero-source');
+                let hero_source = header.querySelector(".hero-source");
                 hero_source.innerHTML = /* html */ `
-                    <a class="hero-source breadcrumbs__a" href="${this.owner.url}">${this.owner.name}</a>`;
+                    <a class="hero-source breadcrumbs__a" href="${
+                        this.owner.url
+                    }">${this.owner.name}</a>`;
 
                 return header.outerHTML;
             })()}
@@ -187,52 +201,68 @@ export default class InfoPageCollector {
             <div class="container">
                 ${await (async () => {
                     // Блок с описанием курса
-                    let course_main_info = document.querySelector('div.course-wrap').cloneNode(true);
+                    let course_main_info = document
+                        .querySelector("div.course-wrap")
+                        .cloneNode(true);
                     // Добавим в него обложку курса
-                    let course_cover = document.createElement('img');
-                    course_cover.className = 'course-wrap-side-cover';
-                    course_cover.src = await this.resourceUrl2base64(this.course_cover);
+                    let course_cover = document.createElement("img");
+                    course_cover.className = "course-wrap-side-cover";
+                    course_cover.src = await this.resourceUrl2base64(
+                        this.course_cover
+                    );
                     course_main_info.appendChild(course_cover);
 
                     // Заголовок описания курса
-                    let course_description = course_main_info.querySelector('.course-wrap-description');
-                    let course_description_header = document.createElement('h2');
-                    course_description_header.className = 'standard-title';
-                    course_description_header.textContent = 'Описание курса';
-                    course_description.insertBefore(course_description_header, course_description.firstChild);
+                    let course_description = course_main_info.querySelector(
+                        ".course-wrap-description"
+                    );
+                    let course_description_header = document.createElement(
+                        "h2"
+                    );
+                    course_description_header.className = "standard-title";
+                    course_description_header.textContent = "Описание курса";
+                    course_description.insertBefore(
+                        course_description_header,
+                        course_description.firstChild
+                    );
 
                     // Клонируем список уроков
-                    let lessons_list = document.querySelector('#lessons-list').cloneNode(true);
-                    let lessons = Array.from(lessons_list.querySelectorAll('.lessons-item'));
+                    let lessons_list = document
+                        .querySelector("#lessons-list")
+                        .cloneNode(true);
+                    let lessons = Array.from(
+                        lessons_list.querySelectorAll(".lessons-item")
+                    );
                     // Из уроков вырезаем прогресс-бары
                     lessons = lessons.map(lesson => {
-                        lesson.querySelector('progress').remove();
+                        lesson.querySelector("progress").remove();
                         return lesson;
                     });
 
                     // Блок со списком уроков
-                    let video_info = document.createElement('div');
-                    video_info.className = 'video_info';
-                    video_info.innerHTML = /* html */ `<h2 class="standard-title">Видео курса</h2>`;
+                    let video_info = document.createElement("div");
+                    video_info.className = "video_info";
+                    video_info.innerHTML = /* html */ ` <h2 class="standard-title" > Видео курса </h2>`;
                     video_info.appendChild(lessons_list);
                     course_main_info.appendChild(video_info);
 
                     // Материалы курса
-                    let materials_info = document.createElement('div');
-                    materials_info.className = 'materials_info';
-                    let materials = document.querySelector('section.section-block.mb-20');                    
-                    if(materials) {
-                        materials.classList.remove('mb-20');
+                    let materials_info = document.createElement("div");
+                    materials_info.className = "materials_info";
+                    let materials = document.querySelector(
+                        "section.section-block.mb-20"
+                    );
+                    if (materials) {
+                        materials.classList.remove("mb-20");
                         materials_info.innerHTML = materials.outerHTML;
                         course_main_info.appendChild(materials_info);
                     }
 
                     return course_main_info.outerHTML;
-                })()}
+                })()}  
             </div>
-            
-        
-        </body>
-        `;
+
+</body>
+`;
     }
 }
